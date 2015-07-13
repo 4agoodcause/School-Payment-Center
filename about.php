14a210b476270f8cad7d0ae34dc063ba48a4aca6
bradsawicki <git@bradsawicki.com>
2015-07-13 16:21:34 -0500

    About tpl

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php include './views/header.php'; ?>
+
+  <div class="main">
+
+    <?php include './views/nav.php'; ?>
+
+    <section class="content">
+
+      <div class="page-header">
+        <h2 class="page-title">About Us</h2>
+      </div>
+
+      <div class="body">
+        <h3>Headline Number Three</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et laoreet ligula. Vestibulum lacinia enim a dapibus ornare. Quisque ultrices risus velit, mattis vulputate ligula varius ut. Aliquam sed mi quis purus vehicula facilisis in et dolor. Suspendisse nec nisl orci. Etiam cursus mollis consequat. Fusce viverra condimentum orci, eu hendrerit eros. Proin commodo condimentum tortor, ac consectetur urna mollis non. Aenean imperdiet nunc sit amet lacus facilisis, at ultrices lectus vulputate. Mauris in dui vitae augue sollicitudin dignissim. Aliquam egestas cursus sem vitae gravida. Suspendisse posuere enim vel dapibus tristique.</p>
+        <h4>Headline Number Four</h4>
+        <p>Suspendisse potenti. Duis suscipit magna eget erat accumsan euismod. Vivamus sodales finibus lacus, eget vehicula nibh hendrerit eget. Nunc dapibus neque a turpis aliquam pretium. Vestibulum consectetur sagittis libero eget hendrerit. Praesent viverra, turpis vitae ornare interdum, felis magna <a href="">link example</a>, vitae ornare urna sem a est. Etiam a nisl ut lacus pretium faucibus vitae non nunc. Proin et enim quis eros viverra auctor. Duis quis diam eu nulla tincidunt porta et ac dui. In hac habitasse platea dictumst. Ut sollicitudin nisl metus, sed gravida est ultrices ac. Vivamus laoreet nisl sit amet eros pellentesque dapibus. Mauris accumsan magna vel nunc iaculis, sed convallis purus tristique. Nullam felis diam, sagittis id lacinia in, luctus eget lacus. Nunc turpis erat, varius ut urna eget, feugiat suscipit libero.</p>
+        <ul>
+          <li>List Item Number one</li>
+          <li>List Item Number two</li>
+          <li>List Item Number three</li>
+        </ul>
+        <p>Duis lobortis, arcu nec dignissim imperdiet, lorem risus consectetur leo, eu ornare neque lorem sit amet nulla. Mauris purus ligula, condimentum vitae sapien sit amet, imperdiet tincidunt odio. Mauris elit dui, elementum in rutrum eget, pellentesque eu erat. Aenean eget quam ut sem pellentesque ullamcorper. Phasellus dignissim, justo nec suscipit efficitur, urna sapien vestibulum dui, vel malesuada nibh dolor at augue. Vivamus accumsan, neque porttitor sollicitudin rutrum, nibh sem dapibus lacus, sit amet maximus nunc ipsum in justo. Sed volutpat dolor nisl, at eleifend nisl sodales sed. Sed et lacinia tortor, quis tristique metus. Sed non urna nec metus placerat efficitur ut in lorem.</p>
+      </div>
+
+    </section>
+
+  </div>
+
+  <?php include './views/footer.php'; ?>
